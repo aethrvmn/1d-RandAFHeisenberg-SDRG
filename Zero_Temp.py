bf8de9ca718ffd96b682db3e5f8bc92b30546384
Vasileios Valatsos <vasilvalat@gmail.com>
2020-05-21 19:57:16 +0300

    Made progress

--- Zero_Temp.py
+++ Zero_Temp.py
@@ -4,19 +4,38 @@ import matplotlib.pyplot as plt
 
 
 #size of 1d lattice
-lattice_size=10
+lattice_size=100
 
 #array of spins
-spin_array=[1/2]
+spin_array=[0.5]
+
+#array of bond strenght
+bonds_array=[]
+
+#Creates the AF state
+for i in np.arange(0,lattice_size,1):
+    if spin_array[i] == 0.5 :
+        spin_array.append((-0.5))
+    else:
+        spin_array.append((0.5))
 
 for i in np.arange(0,lattice_size,1):
-    if spin_array[i] == 1/2 :
-        spin_array.append((-1/2))
-    else:
-        spin_array.append((1/2))
+    J = np.random.uniform(0,10)
+    bonds_array.append(J)
 
-print(spin_array)
-#def hamiltonian(J,S1,S2):
-    #random J
-    #J = np.random.uniform(0,10)
-    
+
+
+#finds the greatest bond
+n=0
+for i in np.arange(0,len(bonds_array),1):
+    m = bonds_array[i]
+    if m > n:
+        x = bonds_array[i-1]
+        y = bonds_array[i]
+        z = bonds_array[i+1]
+        n = y
+
+greatest_bond=[x,y,z]
+
+print(greatest_bond)
+
