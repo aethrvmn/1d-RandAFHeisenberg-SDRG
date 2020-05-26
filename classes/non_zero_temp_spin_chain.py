11edbad970d2d9143da0ddf573124b11f0fd3538
Vasileios Valatsos <vasilvalat@gmail.com>
2020-05-26 21:51:18 +0300

    started non-zero file

---
+++ classes/non_zero_temp_spin_chain.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+'''
+This is the code I have writen to define the Random Antiferromagnetic Heisenberg Spin Chain and the RG transformation (elimination transformation) for a zero temprature system. Currently there is an issue with what happens when the size of the chain falls to zero, and shows an error. I also need to change the distribution method for the bonds so it's an actual random distribution to properly study the RG flow.
+'''
