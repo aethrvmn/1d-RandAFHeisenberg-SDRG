c28118cded702c62cd41b7e943cc763da39ce420
Vasileios Valatsos <vasilvalat@gmail.com>
2020-05-26 20:42:19 +0300

    Updated

--- classes/spin_chain_vasil.py
+++ classes/spin_chain_vasil.py
@@ -46,12 +46,11 @@ class Chain:
         self.bond_prime = (self.left_mini_bond*self.right_mini_bond)/(2*self.mega_bond) # find the strength of the new bond that will exist after we remove the spins
         self.new_local_energy = self.energy_prime + (self.bond_prime*self.state[self.mega_index-1]*self.state[self.mega_index]) # finds the energy that we will add to the total energy after we remove the spins/bonds that existed
         
-        
         self.state = np.delete(self.state, [self.mega_index, self.mega_index+1]) # the new chain after removing the spins sharing the strongest bond
         self.bonds[self.mega_index]=self.bond_prime # replacing the strongest bond with the bond prime
         self.bonds = np.delete(self.bonds, [self.mega_index-1, self.mega_index+1]) # removing the bonds next to the bond prime to get the proper new chain
+        
         self.energy = self.energy - self.local_energy + self.new_local_energy # calculates the new energy of the chain by removing the previous contribution of the strongest bond and adding the new contribution of the newly weak bond in the same spot
-
         self.strong_bond() # refreshes the strongest bond
         return self
         
