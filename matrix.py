2e7683c48271a8b247de80f584bb22bec0c9d67e
Vasilis Valatsos <vasilvalat@gmail.com>
2020-07-02 19:27:15 +0300

    I need to review the bond forming of the RG

--- matrix.py
+++ matrix.py
@@ -6,15 +6,15 @@ from random_singlets.ZT_RSS import ZT_Random_Spin
 
 start_time = time.time()
 
-matrix1 = ZT_Random_Spin(100,10,0.5)
-matrix2 = NZT_Random_Spin(100,10,0.5, 1)
+matrix1 = ZT_Random_Spin(99,1,0.5)
+matrix2 = NZT_Random_Spin(99,1,0.5, 1)
 
-
-print(matrix1.logbonds)
-# matrix1.renormalization()
-# # plt.matshow(matrix1.bond_matrix)
+#print(matrix1.max_bond)
+matrix1.renormalization()
 # # plt.matshow(matrix2.bond_matrix)
-# # plt.show()
 #
 duration = time.time() - start_time
-print("---%s seconds---" %duration)
+#print("---%s seconds---" %duration, matrix1.system_energy)
+plt.matshow(matrix1.bond_matrix)
+plt.show()
+
--- random_singlets/ZT_RSS.py
+++ random_singlets/ZT_RSS.py
@@ -8,17 +8,33 @@ class ZT_Random_Spin:
 
     def __init__(self, number_of_bonds, ceiling, floor):
         self.length = int(number_of_bonds) #the self.length of the chain (actually the total amount of bonds so chain-1)
-        self.bond_matrix = np.zeros(shape=(self.length,self.length)) #initialzes the bond matrix
-        initial_bonds = ceiling*np.random.rand(self.length)
-        np.fill_diagonal(self.bond_matrix, initial_bonds) #adds the bonds
-        A = np.c_[np.zeros(self.length), self.bond_matrix, np.zeros(self.length)] #this allows us to add two columns of 0s, essentially closing the system and also solves IndexErrors for when the max bond is in the boundaries
-        self.bond_matrix = np.r_[[np.zeros(self.length+2)], A, [np.zeros(self.length+2)]] #this adds the rows of 0s, see above
+        self.ceiling = ceiling
+        self.floor = floor
+        # self.bond_matrix = np.zeros(shape=(self.length ,self.length)) #initialzes the bond matrix
+        # initial_bonds = ceiling*np.random.rand(self.length)
+        # np.fill_diagonal(self.bond_matrix, initial_bonds) #adds the bonds
+        # A = np.c_[np.zeros(self.length), self.bond_matrix, np.zeros(self.length)] #this allows us to add two columns of 0s, essentially closing the system and also solves IndexErrors for when the max bond is in the boundaries
+        # A = np.r_[[np.zeros(self.length + 2)], A, [np.zeros(self.length + 2)]] #this adds the rows of 0s, see above
+        # self.bond_matrix = A
+        self.matrix_creator()
         self.average_strength()
         self.bonds()
         self.sys_energy()
         self.logarithmic()
-        self.floor = floor
 
+
+
+    def matrix_creator(self):
+        self.bond_matrix = np.zeros(shape=(self.length ,self.length)) #initialzes the bond matrix
+        initial_bonds = self.ceiling*np.random.rand(self.length)
+        np.fill_diagonal(self.bond_matrix, initial_bonds) #adds the bonds
+        A = np.c_[np.zeros(self.length), self.bond_matrix, np.zeros(self.length)] #this allows us to add two columns of 0s, essentially closing the system and also solves IndexErrors for when the max bond is in the boundaries
+        A = np.r_[[np.zeros(self.length + 2)], A, [np.zeros(self.length + 2)]] #this adds the rows of 0s, see above
+        A = np.c_[np.zeros(self.length + 2), A, np.zeros(self.length + 2)] #this allows us to add two columns of 0s, essentially closing the system and also solves IndexErrors for when the max bond is in the boundaries
+        A = np.r_[[np.zeros(self.length + 4)], A, [np.zeros(self.length + 4)]] #this adds the rows of 0s, see above
+        A = np.c_[np.zeros(self.length + 4), A, np.zeros(self.length + 4)] #this allows us to add two columns of 0s, essentially closing the system and also solves IndexErrors for when the max bond is in the boundaries
+        A = np.r_[[np.zeros(self.length + 6)], A, [np.zeros(self.length + 6)]] #this adds the rows of 0s, see above
+        self.bond_matrix = A
 
     #computes the energy
     def sys_energy(self):
@@ -29,12 +45,13 @@ class ZT_Random_Spin:
     def bonds(self):
         self.max_bond = np.amax(self.bond_matrix)
         self.max_index = np.argwhere(self.bond_matrix.max() == self.bond_matrix).ravel()
-        self.left_index = self.max_index[0]-1
-        self.right_index = self.max_index[0]+1
+        self.left__i_index = self.max_index[0]-1
+        self.right_i_index = self.max_index[0]+1
+        self.left__j_index = self.max_index[1]-1
+        self.right_j_index = self.max_index[1]+1
         self.left_bond = np.sum(self.bond_matrix[self.left_index])
         self.right_bond = np.sum(self.bond_matrix[self.right_index])
         return self
-
 
     #computes the average strength of the bonds
     def average_strength(self):
@@ -43,22 +60,61 @@ class ZT_Random_Spin:
 
     def logarithmic(self):
         self.logmax = -np.log(self.max_bond)
-        self.logbonds = np.log(self.max_bond/self.bond_matrix)
-        self.logbonds[self.logbonds == np.inf] = 0
+        # self.logbonds = np.log(self.max_bond/self.bond_matrix)
+        # self.logbonds[self.logbonds == np.inf] = 0
+
+    def RG_logic(self):
+        #self.bond_matrix[self.bond_matrix == 0.0] = np.nan
+        self.new_rg_spot = self.bond_matrix[self.left__i_index][self.left_j_index] != 0 and self.bond_matrix[self.right_i_index][self.right_j_index] != 0
+        self.right_rg_spot = self.bond_matrix[self.left_i_index][self.left_j_index] != 0 and self.bond_matrix[self.right_i_index][self.right_j_index] == 0
+        self.left_rg_spot = self.bond_matrix[self.left_i_index][self.left_j_index] == 0 and self.bond_matrix[self.right_i_index][self.right_j_index] != 0
+        self.old_rg_spot = self.max_index[0] != self.max_index[1]# and
+        return self
+
     #This is the RG process
     def renormalization(self):
-        #while (self.max_bond > self.floor):
-        energy_prime = -(1/4)*self.max_bond - ((3/16)/self.max_bond)*((self.left_bond**2) + (self.right_bond**2)) # find the energy contribution
-        bond_prime = (self.left_bond*self.right_bond)/(2*self.max_bond) # find the strength of the new bond that will exist after we remove the spins
-        self.local_energy = (-1/4)*np.sum(self.bond_matrix[self.max_index])
-        self.new_local_energy = energy_prime -(1/4)*bond_prime # finds the energy that we will add to the total energy after we remove the spins/bonds that existed
+        while (self.max_bond > self.floor):
+            self.average_strength() # recalculates the average strength
+            self.bonds() # refreshes the strongest bond
+            self.logarithmic()
+            self.RG_logic()
+            energy_prime = -(1/4)*self.max_bond - ((3/16)/self.max_bond)*((self.left_bond**2) + (self.right_bond**2)) # find the energy contribution
+            bond_prime = (self.left_bond*self.right_bond)/(2*self.max_bond) # find the strength of the new bond that will exist after we remove the spins
+            self.local_energy = (-1/4)*np.sum(self.bond_matrix[self.max_index])
+            self.new_local_energy = energy_prime -(1/4)*bond_prime # finds the energy that we will add to the total energy after we remove the spins/bonds that existed
 
-        # self.state = np.delete(self.state, [self.mega_index, self.mega_index+1]) # the new chain after removing the spins sharing the strongest bond
-        # self.bonds[self.mega_index]=self.bond_prime # replacing the strongest bond with the bond prime
-        # self.bonds = np.delete(self.bonds, [self.mega_index-1, self.mega_index+1]) # removing the bonds next to the bond prime to get the proper new chain
+        #Ok, this thing is extremely hard, I have no hopes but here it goes
+        #This it the zero order of RG
+            #This is for the situation where the max bond is in a new area of the chain, i.e. no previous RG transformations.
+            if self.new_rg_spot:
+                self.bond_matrix[self.right_index] = 0
+                self.bond_matrix[self.left_i_index][self.right_j_index] = bond_prime
+                self.bond_matrix[self.left_i_index][self.left_j_index] = 0
+                print('pp1')
+        #This is the first order of RG
+            #This is for the situation where the next bond has been RG transformed.
+            elif self.right_rg_spot:
+                self.bond_matrix[self.right_index] = 0
+                self.bond_matrix[self.left_i_index][self.right_j_index+2] = bond_prime
+                self.bond_matrix[self.left_i_index][self.left_j_index] = 0
+                print('pp2')
+            #This is for the situation where the previous bond has been RG transformed.
+            elif self.left_rg_spot:
+                self.bond_matrix[self.right_index] = 0
+                self.bond_matrix[self.left_i_index-2][self.right_j_index] = bond_prime
+                self.bond_matrix[self.left_i_index][self.left_j_index] = 0
+                print('pp3')
+            elif self.old_rg_spot:
+                self.bond_matrix[self.left_i_index][self.right_j_index + 2] = bond_prime
+                self.bond_matrix[self.right_index + 2] = 0
 
-        self.system_energy = self.system_energy - self.local_energy + self.new_local_energy # calculates the new energy of the chain by removing the previous contribution of the strongest bond and adding the new contribution of the newly weak bond in the same spot
-        self.average_strength() # recalculates the average strength
-        self.bonds() # refreshes the strongest bond
-        self.logarithmic()
+
+
+
+
+
+
+            self.bond_matrix[self.max_index[0]][self.max_index[1]] = self.max_bond - self.ceiling
+            self.system_energy = self.system_energy - self.local_energy + self.new_local_energy # calculates the new energy of the chain by removing the previous contribution of the strongest bond and adding the new contribution of the newly weak bond in the same spot
+
         return self
